1014441360628b3269d1b5c1f10fdce658374da0
Rohan Julka <rohanjulka19@gmail.com>
2024-05-14 11:22:17 +0100

    chnged port added start command

--- server.js
+++ server.js
@@ -72,5 +72,5 @@ app.get('/', (req, res) => {
     res.render(process.env.NODE_ENV === 'production' ? 'index-prod' : 'index');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
